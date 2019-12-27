cb1c7d079cd115bbeb8b70bcefb92d510923d00a
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2019-12-27 12:17:38 -0500

    Fixed linting rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,43 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+  },
   extends: [
     'eslint-config-airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 2019,
-    sourceType: 'module'
-  },
-  env: {
-    node: true
-  },
   overrides: [
     {
       files: ['**/*.test.ts', '**/*.spec.ts', '**/*-spec.ts', 'test/**/*'],
       env: {
-        jest: true
-      }
-    }
+        mocha: true,
+      },
+    },
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
   rules: {
-    // allows for some NestJS practices that show classMethod() { return 'value'; }
-    'class-methods-use-this': 'off',
-    // allow for importing devDependencies in test files
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true
-      }
-    ],
-    // prefer destructed imports instead of importing defaults
-    'import/prefer-default-export': 'off',
-    // allow for dependency injection in NestJS classes
-    'no-useless-constructor': 'off',
-    'no-underscore-dangle': 'off',
     'import/extensions': ['error', { '.ts': 'ignorePackages' }],
-    camelcase: 'off',
-    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    'import/no-duplicates': 'off'
+    'linebreak-style': 'off',
+    'no-confusing-arrow': 'off',
+    'no-multiple-empty-lines': 'error',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': 'error',
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
   },
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.js', '.ts']
-      }
-    }
-  }
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
 };
