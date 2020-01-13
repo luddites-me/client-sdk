85a73423c83f5b26195cc41ac300e3b73de622bf
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-01-13 11:36:42 -0500

    Updated documentation.

--- src/ProtectClient.ts
+++ src/ProtectClient.ts
@@ -4,18 +4,28 @@
 export interface ProtectClient {
   /**
    * Responsible for initiating page navigation to the Platform's Order detail view
+   *
+   * @param orderId - The platform's Order ID to use for constructing the URL to this order page
    */
   navigateToPlatformOrder(orderId: string): void;
+
   /**
    * Initializes the Protect Client SPA in the platform context
    */
   render(): void;
+
   /**
    * Responsible for sizing the SPA to fit the parent container
+   *
+   * @param container - The parent DOM node to which the iframe will be attached
    */
   resizeClientToFitView(container: HTMLElement): void;
 }
 
+/**
+ * TODO: Discover the best place to define these constants.
+ * These are currently defined as strings inside the `ns8-protect-client` client project.
+ */
 export enum ProtectClientEvents {
   ORDER_DETAIL_NAME_CLICK = 'order-detail-name-click',
 }
