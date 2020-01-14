e368eb8a04c289f9316014f23c59035334b37a9e
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-01-14 12:30:22 -0500

    Drop deprecated libTest

--- test/libTest.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import { expect } from 'chai';
-import testMe from '../src/lib';
-
-describe('sample test', () => {
-  it('works!', () => {
-    expect(testMe()).to.equal('hello, world!');
-  });
-});
